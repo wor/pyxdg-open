997027a68821b10fbcda4212e0e55b4ca827c3c5
Esa Määttä <wor@iki.fi>
2014-08-07 22:22:51 +0300

    Prepare for pypi upload

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*- vim:fenc=utf-8:ft=python:et:sw=4:ts=4:sts=4
 from setuptools import setup, find_packages
+from codecs import open # To use a consistent encoding
 import sys
 import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-README = open(os.path.join(here, 'README.rst')).read() if os.path.exists("README.rst") else ""
-NEWS = open(os.path.join(here, 'NEWS.rst')).read() if os.path.exists("NEWS.rst") else ""
+VERSION = open(os.path.join(here, 'VERSION'), encoding="utf-8").read().strip()
+README  = open(os.path.join(here, 'README.rst'), encoding="utf-8").read() if os.path.exists("README.rst") else ""
+NEWS    = open(os.path.join(here, 'NEWS.rst'), encoding="utf-8").read() if os.path.exists("NEWS.rst") else ""
 
-version = '0.1'
+author       ='Esa Määttä'
+author_email ='esa.maatta@iki.fi'
 
 filt_args = []
 exec_name="pyxdg-open"
@@ -19,9 +22,6 @@ for i, arg in enumerate(sys.argv):
         break
 
 console_scripts = [
-        # example entry point in wor/__init__.py:main
-        # 'wor=wor:main'
-        #'tokenize-desktop-file=wor.desktop_file_parser.desktop_file_parser:main',
         '{}=wor.xdg_open:main'.format(exec_name),
         ]
 
@@ -32,34 +32,31 @@ if "--no-console-scripts" in sys.argv[1:]:
 
 
 install_requires = [
-    # List your project dependencies here.
-    # For more details, see:
-    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
     'desktop_file_parser',
     'Magic_file_extensions',
 ]
 
 
 setup(name='pyxdg-open',
-    version=version,
-    description="xdg-open python clone.",
+    version=VERSION,
+    description="Better xdg-open for console use.",
     long_description=README + '\n\n' + NEWS,
-    # Get classifiers from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[c.strip() for c in """
          Development Status :: 4 - Beta
-         License :: OSI Approved :: GNU General Public License (GPL)
-         Operating System :: OS Independent
-         Programming Language :: Python :: 2.7
+         License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)
+         Operating System :: POSIX :: Linux
          Programming Language :: Python :: 3
-         Topic :: Software Development :: Libraries :: Python Modules
+         Topic :: Utilities
          """.split('\n') if c.strip()],
     keywords='python library desktop',
-    author='Esa Määttä',
-    author_email='wor@iki.fi',
-    url='http://github.com/wor/pyxdg-open',
+    author=author,
+    maintainer=author,
+    author_email=author_email,
+    url='https://github.com/wor/pyxdg-open',
     license='GPL3',
     packages=find_packages('src'),
-    package_dir = {'': 'src'},include_package_data=True,
+    package_dir = {'': 'src'},
+    include_package_data=True,
     zip_safe=False,
     # Requirements
     install_requires=install_requires,
